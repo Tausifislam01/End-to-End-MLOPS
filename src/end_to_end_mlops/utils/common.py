da05eefa8b76cb2f5a278443d26a0cc337769159
Tausifislam01 <tausifislam2001@gmail.com>
2026-05-10 17:00:39 +0600

    Add configuration foundation

---
+++ src/end_to_end_mlops/utils/common.py
@@ -0,0 +1,45 @@
+import json
+from pathlib import Path
+from typing import Any
+
+import joblib
+import yaml
+from box import ConfigBox
+from ensure import ensure_annotations
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    with open(path_to_yaml, encoding="utf-8") as yaml_file:
+        content = yaml.safe_load(yaml_file)
+    return ConfigBox(content)
+
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose: bool = True) -> None:
+    for path in path_to_directories:
+        Path(path).mkdir(parents=True, exist_ok=True)
+
+
+@ensure_annotations
+def save_json(path: Path, data: dict) -> None:
+    with open(path, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=4)
+
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    with open(path, encoding="utf-8") as file:
+        content = json.load(file)
+    return ConfigBox(content)
+
+
+@ensure_annotations
+def save_bin(data: Any, path: Path) -> None:
+    joblib.dump(value=data, filename=path)
+
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    return joblib.load(path)
+
